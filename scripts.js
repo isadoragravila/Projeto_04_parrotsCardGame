80f579fdbac2898e1569b877f9835e5a1242191b
Isadora <isadoragravila@gmail.com>
2022-04-09 21:09:20 -0300

    Projeto finalizado sem bônus

--- scripts.js
+++ scripts.js
@@ -4,64 +4,68 @@ let contador = 0;
 let cartasClicadas = [];
 let jogadas = 0;
 
-// pede o número de cartas e continua até que o valor seja válido
+// pede o número de cartas
 let numeroCartas = prompt ("Com quantas cartas você quer jogar?\n\nSOMENTE NÚMEROS PARES!\n(min: 4 e max: 14)\n");
 numeroCartas = Number(numeroCartas);
 
-while (numeroCartas > 14 || numeroCartas < 4 || numeroCartas % 2 === 1 || isNaN(numeroCartas)) {
-    numeroCartas = prompt ("Com quantas cartas você quer jogar?\n\nSOMENTE NÚMEROS PARES!\n(min: 2 e max: 14)\n");
-    numeroCartas = Number(numeroCartas);
-}
 
-// insere os gifs das cartas em um array
-for (let i = 0; i < numeroCartas/2; i++) {
-    embaralhado.push(gifs[i]);
-    embaralhado.push(gifs[i]);
-}
-
-// embaralha o array (ok)
-embaralhado.sort(comparador);
-    function comparador() {
-        return Math.random() - 0.5; 
+function distribuirCartas () {
+    // continua a pedir o número de cartas até que o valor seja válido
+    while (numeroCartas > 14 || numeroCartas < 4 || numeroCartas % 2 === 1 || isNaN(numeroCartas)) {
+        numeroCartas = prompt ("Com quantas cartas você quer jogar?\n\nSOMENTE NÚMEROS PARES!\n(min: 2 e max: 14)\n");
+        numeroCartas = Number(numeroCartas);
     }
-
-// aparece a qtde escolhida de cartas + insere os gifs do array embaralhado nas cartas
-for (let i = 0; i < numeroCartas; i++) {
-    let conteudo = document.querySelector(".conteudo");
-    conteudo.innerHTML += `
-    <div class="carta" onclick="cliqueNaCarta(this)">
-        <div class="front-face face"></div>
-        <div class="back-face face"></div>
-        <img class="frente" src="images/front.png" />
-        <img class="verso transparente" src="${embaralhado[i]}" />
-    </div>`;
+    
+    // insere os gifs das cartas em um array
+    for (let i = 0; i < numeroCartas/2; i++) {
+        embaralhado.push(gifs[i]);
+        embaralhado.push(gifs[i]);
+    }
+    
+    // embaralha o array
+    embaralhado.sort(comparador);
+        function comparador() {
+            return Math.random() - 0.5; 
+        }
+    
+    // aparece a qtde escolhida de cartas + insere os gifs do array embaralhado nas cartas
+    for (let i = 0; i < numeroCartas; i++) {
+        let conteudo = document.querySelector(".conteudo");
+        conteudo.innerHTML += `
+        <div class="carta" onclick="cliqueNaCarta(this)">
+            <div class="front-face face"><img src="images/front.png" /></div>
+            <div class="back-face face"><img src="${embaralhado[i]}" /></div>
+        </div>`;
+    }
 }
 
-//ao clicar o gif aparece, ao clicar em duas, espera 1segundo e chama a função virarCarta
+distribuirCartas ();
+
+//ao clicar o gif aparece, ao clicar em duas, espera 1segundo e chama a função esconderCarta
 function cliqueNaCarta (elemento) {
     if (elemento.classList.contains("clicado") === false) {
         jogadas++;
         elemento.classList.add("clicado");
-        elemento.querySelector(".frente").classList.add("transparente");
-        elemento.querySelector(".verso").classList.remove("transparente");
+        elemento.querySelector(".front-face").classList.add("virar");
+        elemento.querySelector(".back-face").classList.add("virar");
         cartasClicadas.push(elemento.innerHTML);
         contador++;
         if (contador === 2) {
-            setTimeout(virarCarta, 1000);
+            setTimeout(esconderCarta, 1000);
         }
-        let teste = document.querySelectorAll(".frente.transparente");
-        if (teste.length === numeroCartas) {
+        let cartasAbertas = document.querySelectorAll(".front-face.virar");
+        if (cartasAbertas.length === numeroCartas) {
             setTimeout(fimDeJogo, 500);
         }
     }
 }
 
 //se as cartas são iguais, elas permanecem viradas, se elas são diferentes elas desviram
-function virarCarta () {
+function esconderCarta () {
     if (cartasClicadas[0] !== cartasClicadas[1]) {
         while (contador !== 0) {
-            document.querySelector(".clicado").querySelector(".frente").classList.remove("transparente");
-            document.querySelector(".clicado").querySelector(".verso").classList.add("transparente");
+            document.querySelector(".clicado").querySelector(".front-face").classList.remove("virar");
+            document.querySelector(".clicado").querySelector(".back-face").classList.remove("virar");
             document.querySelector(".clicado").classList.remove("clicado");
             contador--;
         }
